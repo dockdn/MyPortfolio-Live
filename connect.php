74a2a3660509e50dcd4fc08f78ba364383cfb358
dockdn <143468675+dockdn@users.noreply.github.com>
2026-07-13 08:54:45 -0400

    Update favicon link path in connect.php

--- connect.php
+++ connect.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>Connect • Danielle</title>
-  <link rel="icon" type="image/png" href="/MyPortfolio/assets/img/favicon.png">
+  <link rel="icon" type="image/png" href="/assets/img/favicon.png">
   <link rel="stylesheet" href="/MyPortfolio/assets/css/style.css?v=107">
 
   <style>
